b5ca10a359c88c7c49893d7d81863129d2eb1722
cecyc <cecycorrea@gmail.com>
2015-06-17 22:33:52 -0500

    added jquery 😭

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,9 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function() {
+    $('#clickMe').on('click', function () {
+    $('h3:first-of-type').toggleClass('highlight');
+  });
+});
